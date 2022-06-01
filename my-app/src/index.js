9eea31db5718fb478a8e1f9cb4d4d7d035082c98
Superdadccs57 <crazytrain401@hotmail.com>
2022-06-01 09:06:00 -0600

    Dived a little deeper into the first component and using JSX instead of React.createElement

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,24 @@
-import ReactDOM  from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+// stateless functional component
+// always  need to return the JSX
+// |
+// V
+
 
 function Greeting() {
-  return <h4>This is Thomas and this is my first component!</h4>;
+  return (
+    <div>
+      <h1>This Heading is nested in a <code>div</code> </h1>
+    </div>
+  );  
 }
+
+// The method below works well for normal html, however will become very messy when introducing nested elements, Like the above code
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'Hello World!');
+// }
 
 ReactDOM.render(<Greeting/>,document.getElementById('root'))
