8786047edac4ab6a0fd1d5113ae245f178fec5e0
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-20 12:00:57 -0600

    react will not render an array with an object, but will render an array with a string and that using the map method you can wrap it in html.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,47 +7,42 @@ const root = createRoot(container);
 
 // Nested Components, React Tools
 // Setup Vars
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/81TFRojp+uL._AC_UY218_.jpg',
-  title: 'Wheel of Time Box Set',
-  author: 'Robert Jordan / Brandon Sanderson',
-  altText: 'Wheel of Time Boxset',
-};
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/51WEFBk27LL._AC_UY218_.jpg',
-  title: 'Options Trading For Dummies',
-  author: 'by Joe Duarte',
-  altText: 'Options Trading for Dummies',
-};
+const books = [
 
+  {
+    img: 'https://m.media-amazon.com/images/I/81TFRojp+uL._AC_UY218_.jpg',
+    title: 'Wheel of Time Box Set',
+    author: 'Robert Jordan / Brandon Sanderson',
+    altText: 'Wheel of Time Boxset',
+  },
+
+  {
+    img: 'https://m.media-amazon.com/images/I/51WEFBk27LL._AC_UY218_.jpg',
+    title: 'Options Trading For Dummies',
+    author: 'by Joe Duarte',
+    altText: 'Options Trading for Dummies',
+  },
+]
+
+const names = ['john', 'peter', 'susan']
+const newName = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newName)
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-        altText={firstBook.altText}
-      >
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum sint dolores cumque, harum quia numquam quidem maxime doloremque illo consequuntur.</p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        altText={secondBook.altText}
-      />
+      {newName}
     </section>
   );
 }
-const Book = ({img,title,author,altText,children}) => {
+const Book = ({img,title,author,altText}) => {
   // const {img, title, author} = props
   return (
     <article className="book">
       <img src={img} alt={altText} />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 }
