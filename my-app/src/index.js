19cf80950d05f96c0ad532dcfe16cdbbe58c35d7
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-20 10:32:44 -0600

    cleaned up code for next lesson

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,35 +13,26 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
   return <article className='book'>
-    <Image />
-    <Title />
-    <Author/>
+    
+    <img
+        src="https://m.media-amazon.com/images/I/81TFRojp+uL._AC_UY218_.jpg"
+        alt="Wheel of Time Boxset"
+      />
+    
+    
+  <h1>Wheel of Time Box Set</h1>
+    
+    
+    <h4>Robert Jordan / Brandon Sanderson</h4>
   </article>
 }
 
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/81TFRojp+uL._AC_UY218_.jpg"
-    alt="Wheel of Time Boxset"
-  />
-);
-
-const Title = () => <h1>Wheel of Time Box Set</h1>;
-
-// This line has JSX-CSS added to the h4!
-const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop:'.25rem'}}>Robert Jordan / Brandon Sanderson</h4>
 
 
 
