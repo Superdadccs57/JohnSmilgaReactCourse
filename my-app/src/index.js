f1674a9363368dd9a98b1e8bc1056579bbbe0f09
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-20 13:45:41 -0600

    learned how to add an onClick event and an onMouseOver event using different types of function options

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -43,16 +43,27 @@ function BookList() {
     </section>
   );
 }
-const Book = (props) => {
-  const {img, title, author,altText} = props
+const Book = ({ img, title, author, altText }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert('Hello World')
+  }
+  const complexExample = (author) => {
+    console.log(author)
+  }
   return (
-    <article className="book">
+    <article className="book" onMouseOut={() => {
+      console.log(title)
+    }}>
       <img src={img} alt={altText} />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>Reference Example</button>
+      <button type='button' onClick={() => complexExample(author)}>more complex example</button>
     </article>
   );
-}
+};
 
 
 
