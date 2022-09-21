853bc013e26825b77f71faf5b7c4864e8035c694
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-21 16:36:25 -0600

    reviewed that using a hook within a conditional is not allowed, however you can set the conditional up within the hook. We stopped the app from displaying 0 in the title

--- AdvancedReact/react-advanced-v18/src/App.js
+++ AdvancedReact/react-advanced-v18/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics'
+import React from 'react';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
