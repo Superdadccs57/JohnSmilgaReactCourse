397f4e842e740b7c6d54d09c6ec8d81e5e4177c3
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-22 14:32:46 -0600

    quickly fixed the quote icon to show

--- AdvancedReact/react-projects/03-reviews/setup/src/Review.js
+++ AdvancedReact/react-projects/03-reviews/setup/src/Review.js
@@ -10,6 +10,7 @@ const Review = () => {
       <div className="img-container">
         <img src={image} alt={name} className="person-img" />
         <span className="quote-icon">
+          <FaQuoteRight />
         </span>
       </div>
       <h4 className="author">{name}</h4>
