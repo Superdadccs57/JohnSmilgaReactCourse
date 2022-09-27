f8908f92de94e376d24c2b5b19f092d63325058b
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-27 12:29:36 -0600

    setup the buttons dynamically so that the buttons will filter through the menu based on the category of the menu

--- AdvancedReact/react-projects/05-menu/setup/src/App.js
+++ AdvancedReact/react-projects/05-menu/setup/src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+
 function App() {
-  const [menuItems, setMenuItems] = useState(items)
-  const [categories,setCategories] = useState([])
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
@@ -13,11 +25,11 @@ function App() {
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu items={menuItems}/>
+        <Categories categories={ categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
-</main>
-  )
+    </main>
+  );
 }
 
 export default App;
