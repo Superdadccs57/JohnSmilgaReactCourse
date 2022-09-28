bb34a172a1a46c03ddcf0bce75f007be716344c2
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-28 11:11:52 -0600

    created some logic to limit the amount of paragraphs that can be displayed with a negative count or a count too large for the data set

--- AdvancedReact/react-projects/08-lorem-ipsum/setup/src/App.js
+++ AdvancedReact/react-projects/08-lorem-ipsum/setup/src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello world');
+    let amount = parseInt(count)
+    if (count <= 0) {
+  amount =1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0,amount))
   }
 
   return (
@@ -24,7 +31,9 @@ function App() {
         <button type='submit' className='btn'>generate</button>
       </form>
       <article className='lorem-text'>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, neque.</p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
       </article>
     </section>
   );
