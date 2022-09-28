89a17bef6297278ee1e071a6349b4bd0926fcc96
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-28 12:38:37 -0600

    created a function to copy color of card to clipboard and set an alert that disapeared after 1 sec, also set the default color so it is displaying default color all the time

--- AdvancedReact/react-projects/09-color-generator/setup/src/App.js
+++ AdvancedReact/react-projects/09-color-generator/setup/src/App.js
@@ -6,12 +6,12 @@ import Values from 'values.js';
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#0000ff').all(5));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      let colors = new Values(color).all(10);
+      let colors = new Values(color).all(5);
       setList(colors);
       setError(false);
       console.log(colors);
@@ -29,7 +29,7 @@ function App() {
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder="#f15025"
+            placeholder="#0000ff"
             className={`${error ? 'error' : null}`}
           ></input>
           <button className="btn" type="submit">
--- AdvancedReact/react-projects/09-color-generator/setup/src/SingleColor.js
+++ AdvancedReact/react-projects/09-color-generator/setup/src/SingleColor.js
@@ -6,10 +6,18 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(',')
   // const hex = rgbToHex(...rgb)
   const hexValue = `#${hexColor}`
-  return <article className={`color ${index > 10 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 1000)
+    return () => {clearTimeout(timeout)}
+  },[alert])
+  return <article className={`color ${index > 10 && 'color-light'}`}
+    style={{ backgroundColor: `rgb(${bcg})` }}
+    onClick={() => { setAlert(true); navigator.clipboard.writeText(hexValue) }}>
     <p className='percent-value'>{weight}%</p>
     <p className="color-value">{hexValue}</p>
-
+{alert && <p className='alert'>copied to clipboard</p>}
   </article>
 }
 
