b84add48697f87c8898a9a2613436a6de56c4a26
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-29 15:52:48 -0600

    setup our global context for the project

--- AdvancedReact/react-projects/13-stripe-submenus/setup/src/context.js
+++ AdvancedReact/react-projects/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,33 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+    // State Values
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+   
+   
+
+    // Functions
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+     
+    };
+    const openSubmenu = () => {
+      setIsSubmenuOpen(true);
+    };
+    const closeSubmenu = () => {
+      
+    };
+    return <AppContext.Provider value={{isSubmenuOpen,isSidebarOpen,openSubmenu,openSidebar,closeSubmenu,closeSidebar}}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
