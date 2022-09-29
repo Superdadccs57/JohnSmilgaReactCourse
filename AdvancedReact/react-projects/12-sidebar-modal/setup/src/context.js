d108bc9e0e90562af0893cdab673b03bc22a970d
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-29 13:16:41 -0600

    setup a useGlobalContext custom hook in a context.js file

--- AdvancedReact/react-projects/12-sidebar-modal/setup/src/context.js
+++ AdvancedReact/react-projects/12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,13 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+    return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+}
+// Custom Hook
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppProvider , AppContext};
