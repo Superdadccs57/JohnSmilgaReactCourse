503b0e9fae69e37051a564b2d18133d99771777f
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-29 10:31:10 -0600

    refactored the app to use the useReduce hook!

--- AdvancedReact/react-advanced-v18/src/tutorial/6-useReducer/setup/index.js
+++ AdvancedReact/react-advanced-v18/src/tutorial/6-useReducer/setup/index.js
@@ -2,28 +2,28 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = (state, action) => {};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: 'hello world',
+};
 
 const Index = () => {
   // React Hooks
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
   // Functions
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true)
-      setPeople([...people, { id: new Date().getTime().toString(), name }])
-      setName('')
-    }
-    else {
-      setShowModal(true)
+    } else {
     }
   };
   return (
     <>
-      {showModal && <Modal />}
-      <form onSubmit={handleSubmit} className='form'>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
         <div>
           <input
             type="text"
@@ -31,12 +31,14 @@ const Index = () => {
             onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <button type='submit'>add</button>
+        <button type="submit">add</button>
       </form>
-      {people.map((person) => {
-        return <div key={person.id}>
-          <h4>{person.name}</h4>
-        </div>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
       })}
     </>
   );
