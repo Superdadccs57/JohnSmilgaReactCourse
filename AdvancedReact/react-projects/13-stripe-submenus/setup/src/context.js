96e141e6fcf5408fc1b06c3dbdc595ec5b52a3e0
Superdadccs57 <crazytrain401@hotmail.com>
2022-09-30 12:20:07 -0600

    finished the project by closing the submenu!

--- AdvancedReact/react-projects/13-stripe-submenus/setup/src/context.js
+++ AdvancedReact/react-projects/13-stripe-submenus/setup/src/context.js
@@ -1,37 +1,49 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
 
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-    // State Values
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
-  const [location, setLocation] = useState({})
-  const [page,setPage] = useState({page:'',links:[]})
-   
+  // State Values
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({ page: '', links: [] });
 
-    // Functions
-    const openSidebar = () => {
-        setIsSidebarOpen(true)
-    }
-    const closeSidebar = () => {
-     setIsSidebarOpen(false)
-    };
+  // Functions
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
   const openSubmenu = (text, coordinates) => {
-    const page = sublinks.find((link) => link.page === text)
-    setPage(page)
-      setLocation(coordinates)
-      setIsSubmenuOpen(true);
-    };
-    const closeSubmenu = () => {
-      setIsSubmenuOpen(false)
-    };
-    return <AppContext.Provider value={{isSubmenuOpen,isSidebarOpen,openSubmenu,openSidebar,closeSubmenu,closeSidebar,location,page,}}>
-        {children}
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+        location,
+        page,
+      }}
+    >
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
