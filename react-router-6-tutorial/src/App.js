7c497d3249977425868a5009080b65bc74b56849
Superdadccs57 <crazytrain401@hotmail.com>
2022-10-07 12:00:03 -0600

    learned about nested route, not really sure how it works just yet

--- react-router-6-tutorial/src/App.js
+++ react-router-6-tutorial/src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
-import Error from './pages/Error'
+import Error from './pages/Error';
 
 function App() {
   return (
     <BrowserRouter>
       <nav>our navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
       <footer>our footer</footer>
     </BrowserRouter>
