1cde5c1c263d531466050b48ec0288ec11af490b
Superdadccs57 <crazytrain401@hotmail.com>
2022-10-07 11:39:19 -0600

    imported link from react-router-dom so that you can navigate internaly to you app, need to still use the conventional <a> to navigate to a webpage other than yours

--- react-router-6-tutorial/src/pages/Products.js
+++ react-router-6-tutorial/src/pages/Products.js
@@ -1,8 +1,11 @@
+import {Link} from 'react-router-dom'
 const Products = () => {
   return (
     <>
-      <section className='section'>
+      <section className="section">
         <h2>products</h2>
+        <Link to='/' className='btn'>Back Home</Link>
+        <Link to='/about' className='btn'>About</Link>
       </section>
     </>
   );
