e47f684e2baa6919edd225f499545271b591883a
Superdadccs57 <crazytrain401@hotmail.com>
2022-10-08 12:34:04 -0600

    Setup the routes for the pages of the app

--- AdvancedReact/react-projects/15-cocktails/setup/src/App.js
+++ AdvancedReact/react-projects/15-cocktails/setup/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
@@ -9,10 +9,24 @@ import Error from './pages/Error'
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail/>
+        </Route>
+        <Route  path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App
