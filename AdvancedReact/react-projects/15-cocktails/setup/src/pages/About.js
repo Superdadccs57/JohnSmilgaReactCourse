385bea3f19c4794529e70ef74f5c1086d12fb862
Superdadccs57 <crazytrain401@hotmail.com>
2022-10-08 12:47:19 -0600

    got the error page and the about page out of the way!

--- AdvancedReact/react-projects/15-cocktails/setup/src/pages/About.js
+++ AdvancedReact/react-projects/15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam eius impedit ipsa minima et dolore, rerum a magni quis deleniti iste esse animi repellat fugit ad error optio quos at minus id ut accusamus. Rerum corporis, velit commodi temporibus tempora eum amet, sapiente fugit aliquam quisquam quo quos modi facere!</p>
+    </section>
   )
 }
 
