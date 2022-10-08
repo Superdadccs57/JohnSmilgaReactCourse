babc67de4d39ef645c9836b098296e8105b727c5
Superdadccs57 <crazytrain401@hotmail.com>
2022-10-07 19:36:06 -0600

    Refactored the products page into a nested route

--- react-router-6-tutorial/src/App.js
+++ react-router-6-tutorial/src/App.js
@@ -5,20 +5,23 @@ import About from './pages/About';
 import Products from './pages/Products';
 import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
-import SingleProduct from './pages/SingleProduct'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import ProtectedRoute from './pages/ProtectedRoute'
+import SingleProduct from './pages/SingleProduct';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './pages/ProtectedRoute';
+import SharedProductLayout from './pages/SharedProductsLayout';
 function App() {
-const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="products" element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=":productId" element={<SingleProduct />} />
+          </Route>
           <Route path="login" element={<Login setUser={setUser} />} />
           <Route
             path="dashboard"
