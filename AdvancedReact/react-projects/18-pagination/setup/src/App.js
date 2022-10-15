31e6f192d450c8c3436ca7a8f6ece90d15296ea2
Thomas Fentie <crazytrain401@hotmail.com>
2022-10-15 12:05:28 -0600

    added the next and prev buttons to cycle through the pages!

--- AdvancedReact/react-projects/18-pagination/setup/src/App.js
+++ AdvancedReact/react-projects/18-pagination/setup/src/App.js
@@ -9,11 +9,31 @@ function App() {
 	useEffect(() => {
 		if (loading) return;
 		setFollowers(data[page]);
-	}, [loading,page]);
+	}, [loading, page]);
 
-	const handlePage = (index) =>{
-		setPage(index)
-	}
+	const handlePage = (index) => {
+		setPage(index);
+	};
+
+	const nextPage = () => {
+		setPage((oldPage) => {
+			let nextPage = oldPage + 1;
+			if (nextPage > data.length - 1) {
+				nextPage = 0;
+			}
+			return nextPage;
+		});
+	};
+
+	const prevPage = () => {
+		setPage((oldPage) => {
+			let prevPage = oldPage - 1;
+			if (prevPage < 0) {
+				prevPage = data.length - 1;
+			}
+			return prevPage;
+		});
+	};
 	return (
 		<>
 			<main>
@@ -27,12 +47,28 @@ function App() {
 							return <Follower key={follower.id} {...follower} />;
 						})}
 					</div>
-					{!loading && <div className="btn-container">
-						{data.map((item,index)=>{
-							return <button className={`page-btn ${index === page?'active-btn':null}`} key={index} onClick={()=>handlePage(index)}>{index + 1}</button>
-						})}
-					</div> }
-					
+					{!loading && (
+						<div className='btn-container'>
+							<button className='prev-btn' onClick={prevPage}>
+								prev
+							</button>
+							{data.map((item, index) => {
+								return (
+									<button
+										className={`page-btn ${
+											index === page ? "active-btn" : null
+										}`}
+										key={index}
+										onClick={() => handlePage(index)}>
+										{index + 1}
+									</button>
+								);
+							})}
+							<button className='next-btn' onClick={nextPage}>
+								next
+							</button>
+						</div>
+					)}
 				</section>
 			</main>
 		</>
