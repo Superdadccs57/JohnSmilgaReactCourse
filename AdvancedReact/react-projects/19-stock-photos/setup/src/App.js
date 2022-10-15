54e9e521711f027e56176b46ed65e5fd24fa388d
Thomas Fentie <crazytrain401@hotmail.com>
2022-10-15 15:33:02 -0600

    Displayed photos and information.

--- AdvancedReact/react-projects/19-stock-photos/setup/src/App.js
+++ AdvancedReact/react-projects/19-stock-photos/setup/src/App.js
@@ -6,7 +6,6 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 
-
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
@@ -16,16 +15,41 @@ function App() {
     try {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data);
+      setPhotos(data)
+      setLoading(false)
     } catch (error) {
       setLoading(false)
       console.log(error);
     }
   }
- useEffect(()=>{
-  fetchImages()
- },[])
-  return <h2>stock photos starter</h2>
+  useEffect(()=>{
+    fetchImages()
+  },[])
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('hello');
+  }
+
+ 
+  return <>
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder='search' className='form-input' />
+          <button type='submit' className='submit-btn' onClick={handleSubmit}><FaSearch/></button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image,index)=>{
+            return <Photo key={image.id} {...image}/>
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  </>
 }
 
 export default App
