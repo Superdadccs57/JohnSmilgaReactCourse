e631b12cdf6e29a7045ba55e03a660a5ffebf44f
Thomas Fentie <crazytrain401@hotmail.com>
2022-10-15 19:16:12 -0600

    setup the search input

--- AdvancedReact/react-projects/19-stock-photos/setup/src/App.js
+++ AdvancedReact/react-projects/19-stock-photos/setup/src/App.js
@@ -9,16 +9,30 @@ function App() {
 	const [loading, setLoading] = useState(false);
 	const [photos, setPhotos] = useState([]);
 	const [page, setPage] = useState(1);
+	const [query, setQuery] = useState("");
+
 	const fetchImages = async () => {
+		setLoading(true);
 		let url;
 		const urlPage = `&page=${page}`;
-		url = `${mainUrl}${clientID}${urlPage}`;
+		const urlQuery = `&query=${query}`;
+
+		if (query) {
+			url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+		} else {
+			url = `${mainUrl}${clientID}${urlPage}`;
+		}
 		try {
 			const response = await fetch(url);
 			const data = await response.json();
-			setPhotos((oldPhotos) =>{
-        return [...oldPhotos, ...data]
-      });
+      console.log(data);
+			setPhotos((oldPhotos) => {
+				if(query){
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
+			});
 			setLoading(false);
 		} catch (error) {
 			setLoading(false);
@@ -35,9 +49,9 @@ function App() {
 				!loading &&
 				window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
 			) {
-				setPage((oldPage)=>{
-          return oldPage + 1
-        })
+				setPage((oldPage) => {
+					return oldPage + 1;
+				});
 			}
 		});
 		return () => window.removeEventListener("scroll", event);
@@ -45,7 +59,7 @@ function App() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log("hello");
+		fetchImages()
 	};
 
 	return (
@@ -53,7 +67,15 @@ function App() {
 			<main>
 				<section className='search'>
 					<form className='search-form'>
-						<input type='text' placeholder='search' className='form-input' />
+						<input
+							type='text'
+							placeholder='search'
+							className='form-input'
+							value={query}
+							onChange={(e) => {
+								setQuery(e.target.value);
+							}}
+						/>
 						<button type='submit' className='submit-btn' onClick={handleSubmit}>
 							<FaSearch />
 						</button>
