259f0510c9acdc207c9d37803f7071045a69a48f
Thomas Fentie <crazytrain401@hotmail.com>
2022-10-15 18:54:24 -0600

    created the code so that we would get our infinite photos.

--- AdvancedReact/react-projects/19-stock-photos/setup/src/App.js
+++ AdvancedReact/react-projects/19-stock-photos/setup/src/App.js
@@ -8,13 +8,17 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
 	const [loading, setLoading] = useState(false);
 	const [photos, setPhotos] = useState([]);
+	const [page, setPage] = useState(1);
 	const fetchImages = async () => {
 		let url;
-		url = `${mainUrl}${clientID}`;
+		const urlPage = `&page=${page}`;
+		url = `${mainUrl}${clientID}${urlPage}`;
 		try {
 			const response = await fetch(url);
 			const data = await response.json();
-			setPhotos(data);
+			setPhotos((oldPhotos) =>{
+        return [...oldPhotos, ...data]
+      });
 			setLoading(false);
 		} catch (error) {
 			setLoading(false);
@@ -23,12 +27,17 @@ function App() {
 	};
 	useEffect(() => {
 		fetchImages();
-	}, []);
+	}, [page]);
 
 	useEffect(() => {
 		const event = window.addEventListener("scroll", () => {
-			if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
-				console.log("it workded");
+			if (
+				!loading &&
+				window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+			) {
+				setPage((oldPage)=>{
+          return oldPage + 1
+        })
 			}
 		});
 		return () => window.removeEventListener("scroll", event);
