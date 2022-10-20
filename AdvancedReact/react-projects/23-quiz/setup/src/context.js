c92d62ce090d0b4fe5821cc3569a04f189106810
Thomas Fentie <crazytrain401@hotmail.com>
2022-10-20 09:52:58 -0600

    setup all our state values in the context.js and put them into the AppContext Provider to use a global context and set up a few conditional renders in the app.js

--- AdvancedReact/react-projects/23-quiz/setup/src/context.js
+++ AdvancedReact/react-projects/23-quiz/setup/src/context.js
@@ -15,7 +15,17 @@ const temptUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=e
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [waiting, setWaiting] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [index, setIndex] = useState(0)
+  const [correct, setCorrect] = useState(0)
+  const [error, setError] = useState(false)
+
+const [isModalOpen, setIsModalOpen] = useState(false)
+
+
+  return <AppContext.Provider value={{waiting,loading,questions,index,correct,error,isModalOpen}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
