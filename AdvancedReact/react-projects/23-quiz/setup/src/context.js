03feb2163883cb11b3c8bb4c7cc954093aa5ba33
Thomas Fentie <crazytrain401@hotmail.com>
2022-10-20 10:17:01 -0600

    used conditional statements to render page depending on successful data response or unsuccesful data response

--- AdvancedReact/react-projects/23-quiz/setup/src/context.js
+++ AdvancedReact/react-projects/23-quiz/setup/src/context.js
@@ -10,7 +10,7 @@ const table = {
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
-const temptUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
 
@@ -23,6 +23,31 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false)
 
 const [isModalOpen, setIsModalOpen] = useState(false)
+
+const fetchQuestions = async (url) => {
+  setLoading(true)
+  setWaiting(false)
+  const response = await axios(url).catch((err)=>console.log(err))
+  if(response){
+    const data = response.data.results
+    if(data.length > 0){
+      setQuestions(data)
+      setLoading(false)
+      setWaiting(false)
+      setError(false)
+    }else {
+      setWaiting(true)
+      setError(true)
+    }
+  }
+  else {
+    setWaiting(true)
+  }
+}
+
+useEffect(()=>{
+  fetchQuestions(tempUrl)
+},[])
 
 
   return <AppContext.Provider value={{waiting,loading,questions,index,correct,error,isModalOpen}}>{children}</AppContext.Provider>
