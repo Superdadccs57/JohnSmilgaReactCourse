783799480af21d0482fc7fc75e2cbb71bffbd0fb
Thomas Fentie <crazytrain401@hotmail.com>
2022-11-01 16:51:21 -0600

    use the Promise.allSettled method to wait for all data back then you can display them at the same time

--- GitHub_Search_Project/src/context/context.js
+++ GitHub_Search_Project/src/context/context.js
@@ -31,14 +31,20 @@ const GithubProvider = ({ children }) => {
 		if (response) {
 			setGithubUser(response.data);
 			const { login, followers_url } = response.data;
-			// repos
-			axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
-				setRepos(response.data)
-			);
-			// followers
-			axios(`${rootUrl}/users/${login}/followers`).then((response) =>
-				setFollowers(response.data)
-			);
+			await Promise.allSettled([
+				axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+				axios(`${rootUrl}/users/${login}/followers`)
+			]).then((results)=>{
+				const [repos, followers] = results
+				const status = 'fulfilled'
+				if(repos.status === status) {
+					setRepos(repos.value.data)
+				}
+				if(followers.status === status) {
+					setFollowers(followers.value.data)
+				}
+			})
+			
 		} else {
 			toggleError(true, "there is no user with that user name");
 		}
