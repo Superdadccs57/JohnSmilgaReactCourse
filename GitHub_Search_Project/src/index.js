d83da81abc401d0a9d958fa53c8dc79eccc135fc
Thomas Fentie <crazytrain401@hotmail.com>
2022-11-08 08:48:36 -0700

    worked on all the code for the auth0 library, setting up an account and building functionality for logins

--- GitHub_Search_Project/src/index.js
+++ GitHub_Search_Project/src/index.js
@@ -6,11 +6,18 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+
+
 ReactDOM.render(
 	<React.StrictMode>
-		<GithubProvider>
-			<App />
-		</GithubProvider>
+		<Auth0Provider
+			domain ="dev-barmbihfg5scza6q.us.auth0.com"
+			clientId="JfpON2Zn6j9sMpSXG9iYU2PFVuvJ1RPO"
+			redirectUri={window.location.origin}>
+			<GithubProvider>
+				<App />
+			</GithubProvider>
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
