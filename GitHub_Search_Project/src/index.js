d017fbcf7163e6c0f755b58e825f2e203d35a697
Thomas Fentie <crazytrain401@hotmail.com>
2022-11-08 09:59:13 -0700

    added the prop cahceLocation='localstorage' to the auth0provider in the index.js to store the social media logins so if the user navigates to a page that doesn't exist and redirects them to a 404 page and they click on the back home button it doesn't just sign them out!

--- GitHub_Search_Project/src/index.js
+++ GitHub_Search_Project/src/index.js
@@ -6,13 +6,12 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-
-
 ReactDOM.render(
 	<React.StrictMode>
 		<Auth0Provider
-			domain ="dev-barmbihfg5scza6q.us.auth0.com"
-			clientId="JfpON2Zn6j9sMpSXG9iYU2PFVuvJ1RPO"
+			domain='dev-barmbihfg5scza6q.us.auth0.com'
+			clientId='JfpON2Zn6j9sMpSXG9iYU2PFVuvJ1RPO'
+			cacheLocation='localstorage'
 			redirectUri={window.location.origin}>
 			<GithubProvider>
 				<App />
